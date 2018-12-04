f236d06b8ba2f650efe067862423c13e5ae3c91a
marister <marister11@gmail.com>
2018-12-04 17:06:26 +0200

    initial changes?

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -164,7 +164,7 @@ module.exports = function(
     command = 'npm';
     args = ['install', '--save', verbose && '--verbose'].filter(e => e);
   }
-  args.push('react', 'react-dom');
+  args.push('react', 'react-dom', 'redux', 'react-redux', 'redux-thunk');
 
   // Install additional template dependencies, if present
   const templateDependenciesPath = path.join(
